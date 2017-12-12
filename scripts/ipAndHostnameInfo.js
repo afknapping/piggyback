cb1df7d6f6393163ab751ab4643b8203e579be66
Fabian <fabian@superpursuitmo.de>
2017-12-12 22:22:00 +0100

    make distribute help output more compact

--- scripts/ipAndHostnameInfo.js
+++ scripts/ipAndHostnameInfo.js
@@ -7,17 +7,13 @@ console.log(" ")
 console.log(" ")
 console.log(clc.yellowBright('started the git daemon and npm-offline'));
 console.log(" ")
-console.log(clc.yellowBright('others can now clone and pull:'));
+console.log(clc.yellowBright('others can now clone, pull and install dependencies from your machine:'));
 console.log(" ")
-console.log(`git clone git://${ip.address()}/ piggyback`)
+console.log(clc.greenBright(`git clone git://${ip.address()}/ piggyback`))
 console.log("cd piggyback ")
 console.log(`git remote add ${os.hostname()} git://${ip.address()}/`)
 console.log(`git pull ${os.hostname()}`)
-console.log(" ")
-console.log(" ")
-console.log(clc.yellowBright('use this machine\'s npm cache as registry:'));
-console.log(" ")
-console.log(`npm install --registry ${ip.address()}`)
+console.log(clc.greenBright(`npm install --registry ${ip.address()}:12644`))
 console.log(" ")
 console.log(" ")
 console.log(clc.yellowBright('finally, here are the processes you might want to kill later :)'));
