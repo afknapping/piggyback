e3d471a096f3883973580de90387d119dc992275
Fabian <fabian@superpursuitmo.de>
2018-02-19 07:10:38 +0100

    add webtorrent demo

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import router from './router'
 import SuiVue from 'semantic-ui-vue'
 import 'semantic-ui-css/semantic.min.css'
 
+var WebTorrent = require('webtorrent')
+
 Vue.use(SuiVue)
 
 Vue.config.productionTip = false
@@ -19,4 +21,16 @@ new Vue({
   template: '<App/>',
   data: {
   }
+})
+
+var client = new WebTorrent()
+
+var torrentId = 'magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent'
+
+client.add(torrentId, function (torrent) {
+  // Torrents can contain many files. Let's use the .mp4 file
+  var file = torrent.files.find(function (file) {
+    return file.name.endsWith('.mp4')
+  })
+  file.appendTo('body') // append the file to the DOM
 })
